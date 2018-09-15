566429e1a577c76996ddb8c4024790f24956d57d
王兵 <pithyone@vip.qq.com>
2018-09-15 09:31:39 +0800

    修改小程序 demo

--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -1,1 +1,25 @@
-Page({})
+Page({
+  data: {
+    array: ['美国', '中国', '巴西', '日本'],
+    index: 0,
+    date: '2016-09-01',
+    time: '12:01',
+  },
+  bindPickerChange(e) {
+    // eslint-disable-next-line no-console
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value,
+    })
+  },
+  bindDateChange(e) {
+    this.setData({
+      date: e.detail.value,
+    })
+  },
+  bindTimeChange(e) {
+    this.setData({
+      time: e.detail.value,
+    })
+  },
+})
